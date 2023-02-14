db62a8706561b8669ee80f9be36d7ac99e9c8b06
Ccharliiee <27688952+Ccharliiee@users.noreply.github.com>
2023-02-13 22:04:17 -0500

    wrap all http requests funcs to a custom hook

--- src/components/NewTask/NewTask.js
+++ src/components/NewTask/NewTask.js
@@ -1,41 +1,25 @@
-import { useState } from 'react';
-
-import Section from '../UI/Section';
-import TaskForm from './TaskForm';
+import Section from "../UI/Section";
+import TaskForm from "./TaskForm";
+import useHttp from "../hooks/useHttp";
 
 const NewTask = (props) => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const { isLoading, error, sendRequest: addTasks } = useHttp();
 
   const enterTaskHandler = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        'https://react-http-6b4a6.firebaseio.com/tasks.json',
-        {
-          method: 'POST',
-          body: JSON.stringify({ text: taskText }),
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        }
-      );
+    addTasks({
+      url: "https://react-http-6b4a6.firebaseio.com/tasks.json",
+      method: "POST",
+      body: { text: taskText },
+      headers: {
+        "Content-Type": "application/json",
+      },
+      rpLoader: (tasksData) => {
+        const generatedId = tasksData.name; // firebase-specific => "name" contains generated id
+        const createdTask = { id: generatedId, text: taskText };
 
-      if (!response.ok) {
-        throw new Error('Request failed!');
-      }
-
-      const data = await response.json();
-
-      const generatedId = data.name; // firebase-specific => "name" contains generated id
-      const createdTask = { id: generatedId, text: taskText };
-
-      props.onAddTask(createdTask);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
-    }
-    setIsLoading(false);
+        props.onAddTask(createdTask);
+      },
+    });
   };
 
   return (
